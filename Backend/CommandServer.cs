981903a0949d04ec8a117ae620784a8774c1920d
jamesbtate <jamesbtate@gmail.com>
2012-11-11 22:44:05 -0500

    barely started on test harness control stuff

---
+++ Backend/CommandServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Backend
+{
+    /// <summary>
+    /// A thread that listens for commands from the test harness. Someone must ask for events from a CommandServer.
+    /// </summary>
+    class CommandServer
+    {
+        
+    }
+}
