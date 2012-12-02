2d1a61ce1b30f66bdc41b850bed8066eb37ab81c
acarwile <csbigal02>
2012-12-02 01:40:05 -0500

    Added newer System.Data.SQLite dll to be compabitable with version four .net. Added core insert, delete, update, replace, select functions to nodedatabase.cs

--- Backend/Database/NodeDatabase.cs
+++ Backend/Database/NodeDatabase.cs
@@ -10,18 +10,10 @@ namespace Backend.Database
 {
     class NodeDatabase
     {
-        /// <summary>Creates a SQLite database. Assumes that the database does not exist already.</summary>
-        /// <param name="pathAndFileName">The path and filename of the SQLite database to create.</param>
-        /// <returns>A SQLiteConnection object representing a connection to a SQLite database.</returns>
+        //Creates a database. Database must not exist.
         public SQLiteConnection CreateDataBase(string pathAndFileName)
         {
-            //The database should not already exist.
-            if (System.IO.File.Exists(pathAndFileName))
-            {
-                throw new Exception("Database already exists!");
-            }
-
-            //This is the connection object. It basically represents the database.
+            //This is the connection object. The database should not already exist.
             SQLiteConnection conn = new SQLiteConnection("Data Source=" + pathAndFileName);
 
             //By opening and closing a connection to a file that does not exist, we create the file.
@@ -31,54 +23,30 @@ namespace Backend.Database
             return conn;
         }
 
-        /// <summary>Returns a connection to an existing database.</summary>
-        /// <param name="pathAndFileName">The path and filename of the existing SQLite database.</param>
-        /// <returns></returns>
+        /// Returns a connection to an existing database. Database must exist.
         public SQLiteConnection ConnectToExistingDatabase(string pathAndFileName)
         {
-            //The database should already exist.
-            if (!System.IO.File.Exists(pathAndFileName))
-            {
-                throw new Exception("Database does not exist!");
-            }
-
-            //This is the connection object. It basically represents the database.
             SQLiteConnection conn = new SQLiteConnection("Data Source=" + pathAndFileName);
             return conn;
         }
 
-        /// <summary>Creates an node table containing node records.</summary>
-        /// <param name="conn">A SQLiteConnection object.</param>
+        //Create a node table. 
         public void CreateNodeTable(SQLiteConnection conn)
         {
-            //This sql query creates a nodes table with uniqueId as the primary key and some other columns.
+            //This sql query creates a nodes table with uniqueId as the primary key.
             string sql = "CREATE TABLE nodes (UniqueId TEXT PRIMARY KEY, Name TEXT, Ip TEXT, Mac TEXT,"
             + " MaxBackupCapacity INTEGER, BackupData INTEGER, NonBackupData INTEGER, FreeSpace INTEGER, TotalCapacity INTEGER,"
             + " RelialibyMetric INTEGER, Hops INTEGER, Smart INTEGER, BackupsFailed INTEGER, BackupsPassed INTEGER, Status TEXT)";
 
-            //SQLiteCommand objects are where we exectute sql statements.
+            //SQLiteCommand objects are where we exectute sql state
             SQLiteCommand cmd = new SQLiteCommand(sql, conn);
-            try
-            {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                conn.Close();
-            }
-            catch (SQLiteException ex)
-            {
-                //if anything is wrong with the sql statement or the database, a SQLiteException will show information about it.
-                Debug.Print(ex.Message);
 
-                //Always make sure the database connectio is closed.
-                if (conn.State == ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-            }
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
 
-        /// <summary>Adds a record into the nodes table.</summary>
-        /// <param name="conn">A SQLiteConnection object.</param>
+        //Insert a new record. Primary key must not be in table already.
         public void InsertNodeRecord(Node n1, SQLiteConnection conn)
         {
             string sql = "INSERT INTO nodes (UniqueId, Name, Ip, Mac, MaxBackupCapacity,"
@@ -104,14 +72,82 @@ namespace Backend.Database
             cmd.Parameters.Add(new SQLiteParameter("@pBackupsPassed", n1.backupsPassed));
             cmd.Parameters.Add(new SQLiteParameter("@pStatus", n1.status));
 
-
-            //Open the connection, exectute the query, and close the connection.
             conn.Open();
             cmd.ExecuteNonQuery();
             conn.Close();
         }
-    }
 
+        //Replace a record.
+        public void ReplaceNodeRecord(Node n1, SQLiteConnection conn)
+        {
+            string sql = "REPLACE INTO nodes (UniqueId, Name, Ip, Mac, MaxBackupCapacity,"
+                + " BackupData, NonBackupData, FreeSpace, TotalCapacity,"
+                + " RelialibyMetric, Hops, Smart, BackupsFailed, BackupsPassed, Status)"
+                + " VALUES (@pUniqueId, @pName, @pIp, @pMac, @pMaxBackupCapacity, @pBackupData, @pNonBackupData, @pFreeSpace"
+                + " @pTotalCapaciy, @pReliablityMetric, @pHops, @pSmart, @pBackupsFailed, @pBackupsPassed, @pStatus)";
+
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            cmd.Parameters.Add(new SQLiteParameter("@pUniqueId", n1.uniqueId));
+            cmd.Parameters.Add(new SQLiteParameter("@pName", n1.name));
+            cmd.Parameters.Add(new SQLiteParameter("@pIp", n1.ip));
+            cmd.Parameters.Add(new SQLiteParameter("@pMac", n1.mac));
+            cmd.Parameters.Add(new SQLiteParameter("@pMaxBackupCapacity", n1.maxBackupCapacity));
+            cmd.Parameters.Add(new SQLiteParameter("@pBackupData", n1.backupData));
+            cmd.Parameters.Add(new SQLiteParameter("@pNonBackupData", n1.nonBackupData));
+            cmd.Parameters.Add(new SQLiteParameter("@pFreeSpace", n1.freeSpace));
+            cmd.Parameters.Add(new SQLiteParameter("@pTotalCapacity", n1.totalCapacity));
+            cmd.Parameters.Add(new SQLiteParameter("@pReliabityMetric", n1.relialibyMetric));
+            cmd.Parameters.Add(new SQLiteParameter("@pHops", n1.hops));
+            cmd.Parameters.Add(new SQLiteParameter("@pSmart", n1.smart));
+            cmd.Parameters.Add(new SQLiteParameter("@pBackkupsFailed", n1.backupsFailed));
+            cmd.Parameters.Add(new SQLiteParameter("@pBackupsPassed", n1.backupsPassed));
+            cmd.Parameters.Add(new SQLiteParameter("@pStatus", n1.status));
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        //Delete a record. Primary key must be in table
+        public void DeleteNodeRecord(Guid id, SQLiteConnection conn)
+        {
+            string sql = "DELETE FROM nodes where Id = @pId";
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            cmd.Parameters.Add(new SQLiteParameter("@pId", id));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        //Get the Hostname in a record. Primary key must be in table already.
+        public string SelectNodeName(Guid id, SQLiteConnection conn)
+        {
+            string sql = "SELECT Name FROM nodes WHERE Id = @pId";
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+            cmd.Parameters.Add(new SQLiteParameter("@pId", id));
+
+            conn.Open();
+            string name = cmd.ExecuteScalar().ToString();
+            conn.Close();
+
+            return name;
+        }
+
+        //Update Hostname in a record. Primary key must be in table already.
+        public void UpdateNodeName(Guid id, string name, SQLiteConnection conn)
+        {
+            string sql = "UPDATE nodes Set Name = @pName WHERE Id = @pId";
+            SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+
+            cmd.Parameters.Add(new SQLiteParameter("@pId", id));
+            cmd.Parameters.Add(new SQLiteParameter("@pName", name));
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+    }
 
         //Defines a node and provides constructor
         public struct Node
