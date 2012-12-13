a9c7188d7965a7b64fea16a4a563396508291b1b
tstal005 <tstal005@odu.edu>
2012-12-12 22:14:00 -0500

    changed backup tab to select directories, adjusted recovery data

--- GUI_FrontEnd/MainForm.cs
+++ GUI_FrontEnd/MainForm.cs
@@ -142,10 +142,10 @@ namespace GUI_FrontEnd
 
         private void btnBackupFiles_Click(object sender, EventArgs e)
         {
-            DialogResult result = openBackupFileDialog.ShowDialog();
+            DialogResult result = openBackupFolderDialog.ShowDialog();
             if (result == DialogResult.OK)
             {
-                string backupFilePath = openBackupFileDialog.FileName;
+                string backupFilePath = openBackupFolderDialog.SelectedPath;
                 try
                 {
                     txtBackupFiles.Text = backupFilePath;
@@ -213,7 +213,7 @@ namespace GUI_FrontEnd
             string[] rowArray = new string[dataGridViewBackupFiles.Rows.Count];
             for (int i = 0; i < (dataGridViewBackupFiles.Rows.Count - 1); i++)
             {
-                rowArray[i] = dataGridViewBackupFiles.Rows[i].Cells[0].Value.ToString();
+                rowArray[i] = (string) dataGridViewBackupFiles.Rows[i].Cells[0].Value;
             }
         }
 
@@ -221,12 +221,11 @@ namespace GUI_FrontEnd
         private void btnRestore_Click(object sender, EventArgs e)
         {
             //Retrieves data about selected recovery file, stores in object {source path, original size, backup time/date}
-            string sourcePath = (string) dataGridViewBackupInfo.Rows[0].Cells["sourcePath"].Value;
-            string size = (string) dataGridViewBackupInfo.Rows[0].Cells["size"].Value;
-            string backupTime = (string) dataGridViewBackupInfo.Rows[0].Cells["backupTime"].Value;
-            string[] recoveryFile = new string[] { sourcePath, size, backupTime };
+            string guid = Backend.Properties.Settings.Default.guid.ToString();
+            string dateAndTime = (string) dataGridViewBackupInfo.Rows[0].Cells["backupTime"].Value;
 
-            string recoveryDestination = txtRecoveryDestination.Text;
+            BackupIndex index = indexDB.GetBackupIndex(guid, dateAndTime);
+            string recoveryDestinationDirectory = txtRecoveryDestination.Text;
         }
 
         private void btnRecoveryRefresh_Click(object sender, EventArgs e)
