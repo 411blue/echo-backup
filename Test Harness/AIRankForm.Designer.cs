d8e6eff9f9e0deb7d13c74969df6f862e3f47408
sbrennanodu <sbren006@odu.edu>
2012-12-12 21:42:33 -0500

    Added some AI unit tests

--- Test Harness/AIRankForm.Designer.cs
+++ Test Harness/AIRankForm.Designer.cs
@@ -33,12 +33,12 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lblName = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtCPU = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtNetwork = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtCPU = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.txtWorstNetwork = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -46,9 +46,12 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtWorstName = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtUnitTests = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstNodes
@@ -89,22 +92,22 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Optimal Node";
             // 
-            // lblName
+            // txtNetwork
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(19, 19);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(41, 13);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "Name: ";
+            this.txtNetwork.Location = new System.Drawing.Point(79, 68);
+            this.txtNetwork.Name = "txtNetwork";
+            this.txtNetwork.ReadOnly = true;
+            this.txtNetwork.Size = new System.Drawing.Size(122, 20);
+            this.txtNetwork.TabIndex = 5;
             // 
-            // txtName
+            // label2
             // 
-            this.txtName.Location = new System.Drawing.Point(79, 16);
-            this.txtName.Name = "txtName";
-            this.txtName.ReadOnly = true;
-            this.txtName.Size = new System.Drawing.Size(122, 20);
-            this.txtName.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Network:";
             // 
             // txtCPU
             // 
@@ -123,22 +126,22 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "CPU:";
             // 
-            // txtNetwork
+            // txtName
             // 
-            this.txtNetwork.Location = new System.Drawing.Point(79, 68);
-            this.txtNetwork.Name = "txtNetwork";
-            this.txtNetwork.ReadOnly = true;
-            this.txtNetwork.Size = new System.Drawing.Size(122, 20);
-            this.txtNetwork.TabIndex = 5;
+            this.txtName.Location = new System.Drawing.Point(79, 16);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(122, 20);
+            this.txtName.TabIndex = 1;
             // 
-            // label2
+            // lblName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Network:";
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(19, 19);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name: ";
             // 
             // groupBox3
             // 
@@ -206,11 +209,30 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "Name: ";
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtUnitTests);
+            this.groupBox4.Location = new System.Drawing.Point(439, 17);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(270, 307);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Unit Tests";
+            // 
+            // txtUnitTests
+            // 
+            this.txtUnitTests.Location = new System.Drawing.Point(16, 19);
+            this.txtUnitTests.Multiline = true;
+            this.txtUnitTests.Name = "txtUnitTests";
+            this.txtUnitTests.Size = new System.Drawing.Size(238, 272);
+            this.txtUnitTests.TabIndex = 0;
+            // 
             // AIRankForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(721, 336);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -222,6 +244,8 @@
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -245,5 +269,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtWorstName;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtUnitTests;
     }
 }
